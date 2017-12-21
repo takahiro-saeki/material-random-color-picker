3128077198474d225ccec18b46b1fe076a8ad469
Saeki, Takahiro <ts-takahiro.saeki@rakuten.com>
2017-12-21 12:00:35 +0900

    update: bug fix.

--- dev/index.js
+++ dev/index.js
@@ -1,7 +1,50 @@
 import mrcp from '../src/js/index.js';
+ 
+document.body.innerHTML = `
+  <style>
+    body {
+      margin: 0;
+    }
+    section {
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+    }
+    header {
+      background: ${mrcp({type: 'darken'})};
+      width: 100%;
+      padding: 1rem;
+      text-align: center;
+    }
+    main {
+      display: flex;
+      flex-direction: row;
+      padding: 1rem;
+      height: 500px;
+      background: ${mrcp({type: 'normal'})};
+    }
+    aside {
+      background: ${mrcp({type: 'lighten', level: 5})};
+      padding: 1rem;
+      flex: 1;
+    }
+    div {
+      background: ${mrcp({type: 'lighten', level: 4})};
+      width: 70%;
+      padding: 1rem;
+    }
+    footer {
+      padding: 1rem;
+      background: ${mrcp({type: 'darken', color: 'blue'})};
+    }
+  </style>
+  <section>
+    <header>title</header>
+    <main>
+      <aside>sidebar</aside>
+      <div>main</div>
+    </main>
+    <footer>footer</footer>
+  </section>
+`
 
-console.log(mrcp({isObject: true, type: 'darken', color: 'red'}))
-console.log(mrcp({isObject: true, level: 5}))
-console.log(mrcp())
-console.log(mrcp())
-console.log(mrcp())
